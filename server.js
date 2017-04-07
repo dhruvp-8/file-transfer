c0f98cb32e58a0bb0e49d4ae1a00e3d9972b0d3a
dhruvp-8 <dhruvpatel5738@gmail.com>
2017-04-07 19:10:50 +0530

    Some Changes!

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var cors = require('cors');
 var morgan = require('morgan');
 
 var port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/'));
-app.use(cors());
 
 app.get('*', function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
