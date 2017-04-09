92553535ae43c1c8ea3cdedee550ebc029f2e9b6
dhruvp-8 <dhruvpatel5738@gmail.com>
2017-04-09 18:55:26 +0530

    CHangin TURN Server

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var peer = new Peer({
 	key: 'saxv2cjyzuz69a4i',
 	 config: {'iceServers': [
-    	{url:'stun:stun01.sipphone.com'},
+    	{url:'stun:stun.iptel.org'},
 		{
-    		url: 'turn:numb.viagenie.ca',
-    		credential: 'muazkh',
-    		username: 'webrtc@live.com'
+		url: 'turn:192.158.29.39:3478?transport=udp',
+		credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+		username: '28224511:1379330808'
 		}
 	]}
 });
